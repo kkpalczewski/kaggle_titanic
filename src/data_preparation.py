58c70a1c244d9cfa5a560505eeafdc8f7cb860ce
kkpalczewki <firstcalmly@gmail.com>
2018-05-19 13:04:44 +0200

    End of data pre-processing

--- src/data_preparation.py
+++ src/data_preparation.py
@@ -15,30 +15,38 @@ data_train_labels = pd.DataFrame(download_data_train, columns = ['PassengerId', 
 download_data_train["trainOrTest"] = "train"
 download_data_test["trainOrTest"] = "test"
 download_data_train_sub = download_data_train.drop(["Survived"], axis=1)
-data_train_test_all = pd.concat([download_data_train_sub, download_data_test])
+data_all = pd.concat([download_data_train_sub, download_data_test])
 
-data_train_test_all["Fare"] = data_train_test_all["Fare"].apply(lambda x: x if not math.isnan(x) else np.mean(data_train_test_all['Fare']))
-data_train_test_all["Sex"] = data_train_test_all["Sex"].apply(lambda x: 1 if x == "male" else 0)
-data_train_test_all["Embarked"] = data_train_test_all["Embarked"].apply(lambda x: 2 if x == "C" else (1 if x == 'Q' else 0))
+################################################ DATA REPLACING ###################################################################
+data_all["Fare"] = data_all["Fare"].apply(lambda x: x if not math.isnan(x) else np.mean(data_all['Fare']))
 
-data_train_test_all['Title'] = data_train_test_all.Name.str.extract(' ([A-Za-z]+)\.', expand=False)
-data_train_test_all['Title'] = data_train_test_all['Title'].replace('Mlle', 'Miss')
-data_train_test_all['Title'] = data_train_test_all['Title'].replace('Ms', 'Miss')
-data_train_test_all['Title'] = data_train_test_all['Title'].replace('Mme', 'Mrs')
-data_train_test_all['Title'] = data_train_test_all['Title'].apply(lambda x: x if x in ['Master', 'Miss', 'Mr', 'Mrs'] else 'Rare')
+data_all["Sex"] = data_all["Sex"].apply(lambda x: 1 if x == "male" else 0)
+
+data_all["Embarked"] = data_all["Embarked"].apply(lambda x: 2 if x == "C" else (1 if x == 'Q' else 0))
+
+################################################ DATA EXTRACTION ##################################################################
+data_all['Title'] = data_all.Name.str.extract(' ([A-Za-z]+)\.', expand=False)
+data_all['Title'] = data_all['Title'].replace('Mlle', 'Miss')
+data_all['Title'] = data_all['Title'].replace('Ms', 'Miss')
+data_all['Title'] = data_all['Title'].replace('Mme', 'Mrs')
+data_all['Title'] = data_all['Title'].apply(lambda x: x if x in ['Master', 'Miss', 'Mr', 'Mrs'] else 'Rare')
 title_map = {'Mrs': 4, 'Miss': 3, 'Master': 2, 'Mr' : 1, 'Rare' : 0}
-data_train_test_all['Title'] = data_train_test_all['Title'].map(title_map)
+data_all['Title'] = data_all['Title'].map(title_map)
 
-data_train_test_all = data_train_test_all.drop(["Ticket"], axis=1)
-data_train_test_all = data_train_test_all.drop(["Cabin"], axis=1)
-data_train_test_all = data_train_test_all.drop(["Name"], axis=1)
+############################################### DATA DROPP #########################################################################
+data_all = data_all.drop(["Ticket"], axis=1)
 
-new_data = data_train_test_all
+data_all = data_all.drop(["Cabin"], axis=1)
+
+data_all = data_all.drop(["Name"], axis=1)
+
+############################################## FILL DATA ##########################################################################
+new_data = data_all
 #new_data['Age'] = np.log(new_data['Age'])
 #age = sns.FacetGrid(new_data, col='Pclass')
 #age.map(plt.hist, 'Age', bins=15)
 
-new_data = pd.DataFrame(data_train_test_all, columns = ['Pclass','Age'])
+new_data = pd.DataFrame(data_all, columns = ['Pclass','Age'])
 new_data["Age"] = new_data["Age"].apply(lambda x: 0 if math.isnan(x) else x)
 new_data = new_data[new_data["Age"] != 0]
 
@@ -73,45 +81,47 @@ for class_prob in prob_sets:
             act_error = new_error
     i += 1
 i = 0
-data_train_test_all["Age"] = data_train_test_all["Age"].apply(lambda x: 0 if math.isnan(x) else x)
-data_train_test_all["Age"] = data_train_test_all.apply(lambda row: kde[row["Pclass"]-1].sample()[0][1] if row["Age"] == 0 else row["Age"], axis = 1)
+data_all["Age"] = data_all["Age"].apply(lambda x: 0 if math.isnan(x) else x)
+data_all["Age"] = data_all.apply(lambda row: kde[row["Pclass"]-1].sample()[0][1] if row["Age"] == 0 else row["Age"], axis = 1)
 
-#print(data_train_test_all.describe())
-#plt.hist(data_train_test_all["Age"], bins=10)
+#print(data_all.describe())
+#plt.hist(data_all["Age"], bins=10)
 #plt.show()
 
-table_check = pd.merge(data_train_labels, data_train_test_all, on=['PassengerId','PassengerId'])
-
+#################################################### CREATE INTERVALS ##############################################################
+table_check = pd.merge(data_train_labels, data_all, on=['PassengerId','PassengerId'])
 table_fare = pd.DataFrame(table_check, columns = ['Fare', 'Survived'])
 table_age = pd.DataFrame(table_check, columns = ['Age', 'Survived'])
 
 table_fare['Fare'] = pd.qcut(table_fare['Fare'], 6)
-print(table_fare.groupby(['Fare'], as_index=False).mean().sort_values(by='Fare', ascending=True))
-data_train_test_all.loc[data_train_test_all['Fare'] <= 7.775, 'Fare'] = 0
-data_train_test_all.loc[data_train_test_all['Fare'] <= 8.662, 'Fare'] = 1
-data_train_test_all.loc[data_train_test_all['Fare'] <= 14.454, 'Fare'] = 2
-data_train_test_all.loc[data_train_test_all['Fare'] <= 26.0, 'Fare'] = 3
-data_train_test_all.loc[data_train_test_all['Fare'] <= 52.369, 'Fare'] = 4
-data_train_test_all.loc[data_train_test_all['Fare'] > 52.369, 'Fare'] = 5
-data_train_test_all['Fare'] = data_train_test_all['Fare'].astype(int)
+fares = [7.775, 8.662, 14.454, 26.0, 52.369]
+data_all.loc[data_all['Fare'] <= fares[0], 'Fare'] = 0
+for i in range(len(fares)-1):
+    data_all.loc[(data_all['Fare'] <= fares[i+1]) & (data_all['Fare'] > fares[i]), 'Fare'] = i+1
+data_all.loc[data_all['Fare'] > fares[len(fares)-1], 'Fare'] = len(fares)
+data_all['Fare'] = data_all['Fare'].astype(int)
 
 table_age['Age'] = pd.qcut(table_age['Age'], 6)
-print(table_age.groupby(['Age'], as_index=False).mean().sort_values(by='Age', ascending=True))
-data_train_test_all.loc[data_train_test_all['Age'] <= 17.822, 'Age'] = 0
-data_train_test_all.loc[data_train_test_all['Age'] <= 22.0, 'Age'] = 1
-data_train_test_all.loc[data_train_test_all['Age'] <= 28.0, 'Age'] = 2
-data_train_test_all.loc[data_train_test_all['Age'] <= 36.868, 'Age'] = 3
-data_train_test_all.loc[data_train_test_all['Age'] <= 43.0, 'Age'] = 4
-data_train_test_all.loc[data_train_test_all['Age'] > 43, 'Age'] = 5
-data_train_test_all['Age'] = data_train_test_all['Age'].astype(int)
+ages = [17.822, 22.0, 28.0, 36.868, 43.0]
+data_all.loc[data_all['Age'] <= ages[0], 'Age'] = 0
+for i in range(len(ages)-1):
+    data_all.loc[(data_all['Age'] <= ages[i+1]) & (data_all['Age'] > ages[i]), 'Age'] = i+1
+data_all.loc[data_all['Age'] > ages[len(ages)-1], 'Age'] = len(ages)
+data_all['Age'] = data_all['Age'].astype(int)
 
-test_data = data_train_test_all[data_train_test_all["trainOrTest"] == 'test']
-train_data = data_train_test_all[data_train_test_all["trainOrTest"] == 'train']
+test_data = data_all[data_all["trainOrTest"] == 'test']
+train_data = data_all[data_all["trainOrTest"] == 'train']
 train_data = pd.merge(train_data, data_train_labels, on=['PassengerId','PassengerId'])
 
 test_data = test_data.drop(["trainOrTest"], axis=1)
 train_data = train_data.drop(["trainOrTest"], axis=1)
 
-test_data.to_csv('.\\..\\data\\test.csv', sep='\t', encoding='utf-8')
-train_data.to_csv('.\\..\\data\\train.csv', sep='\t', encoding='utf-8')
+############################################## SAVE TO CSV ########################################################
+test_data.to_csv('.\\..\\data\\test.csv', sep='\t', encoding='utf-8', index=False)
+train_data.to_csv('.\\..\\data\\train.csv', sep='\t', encoding='utf-8', index=False)
 
+if __name__ == '__main__':
+    print('-'*60)
+    print(train_data.describe())
+    print('-'*60)
+    print(test_data.describe())
