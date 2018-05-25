31f7765c9d37fd85a2889513b6b9cd5d2d589712
kkpalczewski <firstcalmly@gmail.com>
2018-05-25 10:37:34 +0200

    End of data pre-processing

--- src/data_preparation.py
+++ src/data_preparation.py
@@ -4,17 +4,21 @@ import numpy as np
 import math
 from sklearn.neighbors.kde import KernelDensity
 
+PREDICTORS = ['Pclass', 'Title', 'Sex', 'Age', 'SibSp', 'Parch', 'Fare', 'Embarked']
+TARGET = 'Survived'
+ID_COLUMN = 'PassengerId'
+
 # download all data
 download_data_train = pd.read_csv("..\download\\train.csv")
 download_data_test = pd.read_csv("..\download\\test.csv")
 
 #create new labels
-data_train_labels = pd.DataFrame(download_data_train, columns = ['PassengerId', 'Survived'])
+data_train_labels = pd.DataFrame(download_data_train, columns = [ID_COLUMN, TARGET])
 
 #create one table for easier data preparation
 download_data_train["trainOrTest"] = "train"
 download_data_test["trainOrTest"] = "test"
-download_data_train_sub = download_data_train.drop(["Survived"], axis=1)
+download_data_train_sub = download_data_train.drop([TARGET], axis=1)
 data_all = pd.concat([download_data_train_sub, download_data_test])
 
 ################################################ DATA REPLACING ###################################################################
@@ -54,33 +58,40 @@ class1 = new_data[new_data['Pclass'] == 1]
 class2 = new_data[new_data['Pclass'] == 2]
 class3 = new_data[new_data['Pclass'] == 3]
 
-tt = np.random.rand(len(class1)) < 0.8
-class1_train = class1[tt]
-class1_test = class1[~tt]
-
-tt = np.random.rand(len(class2)) < 0.8
-class2_train = class2[tt]
-class2_test = class2[~tt]
-
-tt = np.random.rand(len(class3)) < 0.8
-class3_train = class3[tt]
-class3_test = class3[~tt]
-
-prob_sets = [[class1_train,class1_test],[class2_train,class2_test],[class3_train,class3_test]]
-prob_kernels = ['gaussian','tophat']
+# tt = np.random.rand(len(class1)) < 0.8
+# class1_train = class1[tt]
+# class1_test = class1[~tt]
+#
+# tt = np.random.rand(len(class2)) < 0.8
+# class2_train = class2[tt]
+# class2_test = class2[~tt]
+#
+# tt = np.random.rand(len(class3)) < 0.8
+# class3_train = class3[tt]
+# class3_test = class3[~tt]
+#
+# prob_sets = [[class1_train,class1_test],[class2_train,class2_test],[class3_train,class3_test]]
+# prob_kernels = ['gaussian','tophat']
+# kde = [0,0,0]
+# i = 0
+# for class_prob in prob_sets:
+#     act_error = 10e6
+#     for kernel in prob_kernels:
+#         new_kde = KernelDensity(kernel=kernel, bandwidth=0.2)
+#         new_kde.fit(class_prob[0])
+#         new_error = np.sum(np.matmul(new_kde.score_samples(class_prob[1]),new_kde.score_samples(class_prob[1])))
+#         if new_error < act_error:
+#             kde[i] = new_kde
+#             act_error = new_error
+#     i += 1
 kde = [0,0,0]
 i = 0
-for class_prob in prob_sets:
+for class_prob in [class1, class2, class3]:
     act_error = 10e6
-    for kernel in prob_kernels:
-        new_kde = KernelDensity(kernel=kernel, bandwidth=0.2)
-        new_kde.fit(class_prob[0])
-        new_error = np.sum(np.matmul(new_kde.score_samples(class_prob[1]),new_kde.score_samples(class_prob[1])))
-        if new_error < act_error:
-            kde[i] = new_kde
-            act_error = new_error
+    new_kde = KernelDensity(kernel='gaussian', bandwidth=0.2)
+    new_kde.fit(class_prob)
+    kde[i] = new_kde
     i += 1
-i = 0
 data_all["Age"] = data_all["Age"].apply(lambda x: 0 if math.isnan(x) else x)
 data_all["Age"] = data_all.apply(lambda row: kde[row["Pclass"]-1].sample()[0][1] if row["Age"] == 0 else row["Age"], axis = 1)
 
@@ -111,7 +122,7 @@ data_all['Age'] = data_all['Age'].astype(int)
 
 test_data = data_all[data_all["trainOrTest"] == 'test']
 train_data = data_all[data_all["trainOrTest"] == 'train']
-train_data = pd.merge(train_data, data_train_labels, on=['PassengerId','PassengerId'])
+train_data = pd.merge(train_data, data_train_labels, on=[ID_COLUMN, ID_COLUMN])
 
 test_data = test_data.drop(["trainOrTest"], axis=1)
 train_data = train_data.drop(["trainOrTest"], axis=1)
--- src/model_execution.py
+++ src/model_execution.py
@@ -1,13 +1,10 @@
 import pandas as pd
-import numpy as np
-from sklearn.ensemble import RandomForestClassifier
-import xgboost as xgb
+from xgboost import DMatrix, XGBClassifier, cv
+from xgboost import plot_importance, plot_tree
 import datetime
-from sklearn import cross_validation, metrics
-from sklearn.grid_search import GridSearchCV
-from sklearn.cross_validation import train_test_split
-from sklearn.datasets import dump_svmlight_file
-from sklearn.metrics import precision_score
+from sklearn import metrics
+from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import train_test_split
 from matplotlib import pyplot as plt
 
 PREDICTORS = ['Pclass', 'Title', 'Sex', 'Age', 'SibSp', 'Parch', 'Fare', 'Embarked']
@@ -18,8 +15,8 @@ ID_COLUMN = 'PassengerId'
 def modelfit(alg, dtrain, predictors, target, useTrainCV=True, cv_folds=5, early_stopping_rounds=50):
     if useTrainCV:
         xgb_param = alg.get_xgb_params()
-        xgtrain = xgb.DMatrix(dtrain[predictors].values, label=dtrain[target].values)
-        cvresult = xgb.cv(xgb_param, xgtrain, num_boost_round=alg.get_params()['n_estimators'], nfold=cv_folds,
+        xgtrain = DMatrix(dtrain[predictors].values, label=dtrain[target].values)
+        cvresult = cv(xgb_param, xgtrain, num_boost_round=alg.get_params()['n_estimators'], nfold=cv_folds,
                           metrics='auc', early_stopping_rounds=early_stopping_rounds)
         alg.set_params(n_estimators=cvresult.shape[0])
 
@@ -39,15 +36,12 @@ class XgbMod():
     def __init__(self, params=None):
         if params is None:
             self.params = {"n_estimators": 20, "seed": 41, 'max_depth': 8}
-            self.classifier = xgb.XGBClassifier(self.params)
+            self.classifier = XGBClassifier(self.params)
         else:
             self.params = params
-            self.classifier = xgb.XGBClassifier(**self.params)
+            self.classifier = XGBClassifier(**self.params)
 
-    def trainModel(self, data_train_X, data_train_y):
-        self.data_train_X = data_train_X
-        self.data_train_y = data_train_y
-        data_train = data_train_X.join(data_train_y)
+    def trainModel(self, data_train):
         modelfit(self.classifier, data_train, PREDICTORS, TARGET, useTrainCV=True, cv_folds=5, early_stopping_rounds=50)
 
     def predict(self, data_test_X, data_test_y=None):
@@ -59,8 +53,8 @@ class XgbMod():
         return self.Y_pred
 
     def plotTraining(self):
-        xgb.plot_importance(self.classifier)
-        xgb.plot_tree(self.classifier)
+        plot_importance(self.classifier)
+        plot_tree(self.classifier)
         plt.show()
 
     def savePredictionCSV(self, out_dir):
@@ -69,10 +63,10 @@ class XgbMod():
         out = pd.DataFrame(out, columns=[ID_COLUMN, TARGET])
         out.to_csv(out_dir + time_stamp, sep=',', encoding='utf-8', index=False)
 
-    def makeCVsearch(self, params, data_train_X, data_train_y):
+    def makeCVsearch(self, params, data_train):
         cvsearch = GridSearchCV(estimator=xgbm.classifier, param_grid=params, scoring='roc_auc', n_jobs=4, iid=False,
                              cv=5)
-        cvsearch.fit(data_train_X[PREDICTORS], data_train_y[TARGET])
+        cvsearch.fit(data_train[PREDICTORS], data_train[TARGET])
         for score in cvsearch.grid_scores_:
             print(score)
         print('-' * 30)
@@ -82,25 +76,33 @@ class XgbMod():
 
 
 if __name__=='__main__':
-    data_train = pd.read_csv(filepath_or_buffer="..\\data\\train_with_intervals.csv", delimiter='\t')
+    data_train = pd.read_csv(filepath_or_buffer="..\\data\\train_without_intervals.csv", delimiter='\t')
     data_train_y = pd.DataFrame(data_train[TARGET])
     data_train_X = data_train.drop([TARGET], axis=1)
-    data_test_X = pd.read_csv("..\\data\\test_with_intervals.csv", delimiter='\t')
+    data_test_X = pd.read_csv("..\\data\\test_without_intervals.csv", delimiter='\t')
     split_X_train, split_X_test, split_y_train, split_y_test = train_test_split(data_train_X, data_train_y, test_size=0.2, random_state=42)
 
     #first stap -
     params = {"n_estimators": 1000,
               "seed": 27,
               "max_depth": 2,
-              "min_child_weight": 4,
+              "min_child_weight": 2,
               "gamma": 0.3,
               "colsample_bytree": 0.65,
               "subsample": 0.4,
               "reg_alpha": 0
               }
 
+    # params = {"n_estimators": 1000,
+    #           "seed": 42,
+    #           "max_depth": 5,
+    #           "min_child_weight": 8,
+    #           "gamma": 0.4,
+    #           "colsample_bytree": 0.9,
+    #           "subsample": 0.7,
+    #           "reg_alpha": 0}
     xgbm = XgbMod(params)
-    xgbm.trainModel(data_train_X, data_train_y)
+    xgbm.trainModel(data_train)
     xgbm.predict(data_test_X)
 
     #param_test_1 = {'max_depth': list(range(6, 12, 1)), 'min_child_weight': list(range(6, 10, 1))}
@@ -110,9 +112,9 @@ if __name__=='__main__':
     #param_test_5 = {'reg_alpha': [0, 1e-5, 1e-2, 0.1, 1, 100]}
     #param_test_6 = {'reg_alpha': [0, 1e-10, 1e-6, 1e-5, 1e-4, 1e-5, 1e-4, 1e-3, 1e-2, 1e-1]}
     #param_test_7 = {'learning_rate': [0.01,0.03,0.01,0.03]}
-    #xgbm.makeCVsearch(param_test_1,data_train_X, data_train_y)
+    #xgbm.makeCVsearch(param_test_1,data_train)
 
     xgbm.plotTraining()
 
-    xgbm.savePredictionCSV("..\\analysis\\upload_5_xgb")
+    xgbm.savePredictionCSV("..\\analysis\\upload_8_xgb")
 
